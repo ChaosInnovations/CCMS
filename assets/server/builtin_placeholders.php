3c2bcac7e5aa990e9a991f3b79bc635cfd632568
Thomas Boland <thomas.boland@outlook.com>
2018-09-23 16:16:38 -0700

    Cleanup

--- assets/server/builtin_placeholders.php
+++ assets/server/builtin_placeholders.php
@@ -194,30 +194,31 @@ function module_builtin_contactus_submit() {
 	
 	function ajax_contactform() {
 		global $authuser, $mailer;
-		if (isset($_POST["name"]) and isset($_POST["reply"]) and isset($_POST["message"])) {
-			
-			$htmlbody  = "<h2>Message from {$_POST["name"]}</h2>";
-			$htmlbody .= "<h4>Reply: {$_POST["reply"]}</h4>";
-			$htmlbody .= "<p><strong>Message:</strong></p>";
-			$htmlbody .= "<p>{$_POST["message"]}</p>";
-			$htmlbody .= "<small>This message was sent using the online Contact form.</small>";
-			
-			$body  = "Message from {$_POST["name"]}\n";
-			$body .= "Reply: {$_POST["reply"]}\n";
-			$body .= "================================\n\n";
-			$body .= "Message:\n";
-			$body .= "{$_POST["message"]}\n\n";
-			$body .= "This message was send using the online Contact form.",
-			
-			$mail = $mailer->compose([[getconfig("primaryemail")]], "Message from {$_POST["name"]}", $htmlbody, $body);
-			if ($mail->send()) {
-				return "TRUE";
-			} else {
-				return "FALSE";
-			}
-		} else {
+		if (!isset($_POST["name"]) ||
+		    !isset($_POST["reply"]) ||
+			!isset($_POST["message"])) {
 			return "FALSE";
 		}
+		
+		$htmlbody  = "<h2>Message from {$_POST["name"]}</h2>";
+		$htmlbody .= "<h4>Reply: {$_POST["reply"]}</h4>";
+		$htmlbody .= "<p><strong>Message:</strong></p>";
+		$htmlbody .= "<p>{$_POST["message"]}</p>";
+		$htmlbody .= "<small>This message was sent using the online Contact form.</small>";
+		
+		$body  = "Message from {$_POST["name"]}\n";
+		$body .= "Reply: {$_POST["reply"]}\n";
+		$body .= "================================\n\n";
+		$body .= "Message:\n";
+		$body .= "{$_POST["message"]}\n\n";
+		$body .= "This message was send using the online Contact form.",
+		
+		$mail = $mailer->compose([[getconfig("primaryemail")]], "Message from {$_POST["name"]}", $htmlbody, $body);
+		if (!$mail->send()) {
+			return "FALSE";
+		}
+		
+		return "TRUE";
 	}
 }
 
