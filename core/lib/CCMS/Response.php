74142d178d7e8716ba68d758a3d95b6affc2c98c
Thomas Boland <thomas.boland@outlook.com>
2019-02-17 14:10:03 -0800

    Caching prevention happens in .htaccess now

--- core/lib/CCMS/Response.php
+++ core/lib/CCMS/Response.php
@@ -14,12 +14,6 @@ class Response
     
     public function send()
     {
-        // Prevent caching
-        header("Expires: 0");
-        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-        header("Cache-Control: no-store, no-cache, must-revalidate");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
         
         ob_end_clean();
         ignore_user_abort(true);
