3b7e8d1aa6da85de27fdcff1ffc928af57685ebb
Thomas Boland <thomas.boland@outlook.com>
2019-02-19 18:30:05 -0800

    Moved ContactForm email to templates

--- core/lib/CCMS/Utilities.php
+++ core/lib/CCMS/Utilities.php
@@ -45,4 +45,12 @@ class Utilities
         }
         return new Response("TRUE");
     }
+    
+    public static function fillTemplate(string $template, array $vars)
+    {
+        foreach($vars as $k=>$v){
+            $template = str_replace('{'.$k.'}', $v, $template);
+        }
+        return $template;
+    }
 }
--- core/mod/ContactForm/ContactForm.php
+++ core/mod/ContactForm/ContactForm.php
@@ -4,6 +4,7 @@ namespace Mod;
 
 use \Lib\CCMS\Request;
 use \Lib\CCMS\Response;
+use \Lib\CCMS\Utilities;
 use \Mod\Mailer;
 
 class ContactForm
@@ -14,34 +15,30 @@ class ContactForm
         return $html;
         return urldecode("");
     }
-    
+
     public static function hookFormResponse(Request $request)
     {
         // api/contactform/response
         if (!isset($_POST["name"]) ||
-		    !isset($_POST["reply"]) ||
-			!isset($_POST["message"])) {
-			return new Response("FALSE");
-		}
-		
-		$htmlbody  = "<h2>Message from {$_POST["name"]}</h2>";
-		$htmlbody .= "<h4>Reply: {$_POST["reply"]}</h4>";
-		$htmlbody .= "<p><strong>Message:</strong></p>";
-		$htmlbody .= "<p>{$_POST["message"]}</p>";
-		$htmlbody .= "<small>This message was sent using the online Contact form.</small>";
-		
-		$body  = "Message from {$_POST["name"]}\n";
-		$body .= "Reply: {$_POST["reply"]}\n";
-		$body .= "================================\n\n";
-		$body .= "Message:\n";
-		$body .= "{$_POST["message"]}\n\n";
-		$body .= "This message was send using the online Contact form.";
-		
-		$mail = Mailer::Instance()->compose([[Mailer::Instance()->username]], "Message from {$_POST["name"]}", $htmlbody, $body);
-		if (!$mail->send()) {
-			return new Response("FALSE");
-		}
-		
-		return new Response("TRUE");
+            !isset($_POST["reply"]) ||
+            !isset($_POST["message"])) {
+            return new Response("FALSE");
+        }
+
+        $template_vars = [
+            "name" => $_POST["name"],
+            "reply" => $_POST["reply"],
+            "message" => $_POST["message"],
+        ];
+
+        $htmlbody = Utilities::fillTemplate(file_get_contents(dirname(__FILE__) . "/ContactFormEmail.template.html"), $template_vars);
+        $altbody = Utilities::fillTemplate(file_get_contents(dirname(__FILE__) . "/ContactFormEmail.template.txt"), $template_vars);
+
+        $mail = Mailer::Instance()->compose([[Mailer::Instance()->username]], "Message from {$_POST["name"]}", $htmlbody, $altbody);
+        if (!$mail->send()) {
+            return new Response("FALSE");
+        }
+
+        return new Response("TRUE");
     }
 }
