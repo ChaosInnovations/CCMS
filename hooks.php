11ce3ad7ac6bbc390d62de478866a1a9ea2c7af2
Thomas Boland <thomas.boland@outlook.com>
2019-06-25 17:45:46 -0700

    Added route positions to module manifests

--- hooks.php
+++ hooks.php
@@ -8,58 +8,51 @@
 // Hooks are prefixed by either 'web:' or 'cli:' depending on whether
 // run by a web server or by command line interface (i.e. by cron)
 
+// Route positions:
+// 100 - 199 : Configuration/verification/service start
+// 200 - 299 : Specific-hook routes (i.e API paths)
+// 300 - 999 : Specific-catch routes (i.e all from web, all from web without extension)
+// 1000+     : Non-specific-catch routes (all requests)
+
 $hooks = [
-    ['/.*/',                                       "\Mod\Database::hookOpenConnection"],            // (done) Connect to database
-    ['/.*/',                                       "\Mod\User::hookVerifyConfiguration"],           // (done) Verify database configuration
-    ['/.*/',                                       "\Mod\Collaboration::hookVerifyConfiguration"],  // (done) Verify database configuration
+    ['/.*/',                                       "\Mod\Database::hookOpenConnection"],
+    ['/.*/',                                       "\Mod\User::hookVerifyConfiguration"],
+    ['/.*/',                                       "\Mod\Collaboration::hookVerifyConfiguration"],
+    ['/.*/',                                       "\Mod\FireSock::hookVerifyServer"],
+    ['/^web:.*/',                                  "\Mod\User::hookAuthenticateFromRequest"],
   
-    ['/^cli:\/?firesock\/?$/',                     "\Mod\FireSock::hookStartServer"],               // (done) Start WS Server
-    ['/.*/',                                       "\Mod\FireSock::hookVerifyServer"],              // (done) Check that WS Server is running.
-    ['/^web:\/?api\/firesock\/newlongpoll\/?$/i',  "\Mod\FireSock::hookLongPollToken"],             // (done) FireSock long-polling fallback request token
-    ['/^web:\/?api\/firesock\/longpoll\/?$/i',     "\Mod\FireSock::hookLongPoll"],                  // (done) FireSock long-polling fallback for outbound data (pushes)
-    ['/^web:\/?api\/firesock\/longpoll-in\/?$/i',  "\Mod\FireSock::hookLongPollInbound"],           // (done) FireSock long-polling fallback for inbound data
+    ['/^cli:\/?firesock\/?$/',                     "\Mod\FireSock::hookStartServer"],
+    ['/^web:\/?api\/firesock\/newlongpoll\/?$/i',  "\Mod\FireSock::hookLongPollToken"],
+    ['/^web:\/?api\/firesock\/longpoll\/?$/i',     "\Mod\FireSock::hookLongPoll"],
+    ['/^web:\/?api\/firesock\/longpoll-in\/?$/i',  "\Mod\FireSock::hookLongPollInbound"],
+    ['/^web:\/?api\/checkuser\/?$/i',              "\Mod\User::hookCheckUser"],
+    ['/^web:\/?api\/checkpass\/?$/i',              "\Mod\User::hookCheckPassword"],
+    ['/^web:\/?api\/newtoken\/?$/i',               "\Mod\User\AccountManager::hookNewToken"],
+    ['/^web:\/?api\/user\/new\/?$/i',              "\Mod\User::hookNewUser"],
+    ['/^web:\/?api\/user\/edit\/?$/i',             "\Mod\User::hookEditUser"],
+    ['/^web:\/?api\/user\/remove\/?$/i',           "\Mod\User::hookRemoveUser"],
+    ['/^web:\/?api\/user\/password\/reset\/?$/i',  "\Mod\User::hookPasswordReset"],
+    ['/^web:\/?api\/user\/password\/edit\/?$/i',   "\Mod\User::hookPasswordChange"],
+    ['/^web:\/?api\/siteconfiguration\/set\/?$/i', "\Mod\SiteConfiguration::hookSetConfig"],
+    ['/^web:\/?api\/collab_update\/?$/i',          "\Mod\Collaboration::hookUpdate"],
+    ['/^web:\/?api\/page\/new\/?$/i',              "\Mod\Page::hookNewPage"],
+    ['/^web:\/?api\/page\/remove\/?$/i',           "\Mod\Page::hookRemovePage"],
+    ['/^web:\/?api\/page\/edit\/?$/i',             "\Mod\Page::hookEditPage"],
+    ['/^web:\/?api\/page\/secure\/?$/i',           "\Mod\Page::hookSecurePage"],
+    ['/^web:\/?api\/page\/checkpid\/?$/i',         "\Mod\Page::hookCheckPid"],
+    ['/^web:\/?api\/contactform\/response\/?$/i',  "\Mod\ContactForm::hookFormResponse"],
+    ['/^web:\/?api\/notice\/checknid\/?$/i',       "\Mod\Notice::hookCheckNid"],
+    ['/^web:\/?api\/notice\/new\/?$/i',            "\Mod\Notice::hookNew"],
+    ['/^web:\/?api\/notice\/delete\/?$/i',         "\Mod\Notice::hookDelete"],
   
-    ['/^web:.*/',                                  "\Mod\User::hookAuthenticateFromRequest"],       // (done) Sign in
-  
-    ['/^web:\/?api\/checkuser\/?$/i',              "\Mod\User::hookCheckUser"],                     // (done) Check that the username is correct
-    ['/^web:\/?api\/checkpass\/?$/i',              "\Mod\User::hookCheckPassword"],                 // (done) Check that the password is correct
-    ['/^web:\/?api\/newtoken\/?$/i',               "\Mod\User\AccountManager::hookNewToken"],       // (done) Create a new token if authenticated
-    ['/^web:\/?api\/user\/new\/?$/i',              "\Mod\User::hookNewUser"],                       // (done) Create a new user
-    ['/^web:\/?api\/user\/edit\/?$/i',             "\Mod\User::hookEditUser"],                      // (done) Edit a user
-    ['/^web:\/?api\/user\/remove\/?$/i',           "\Mod\User::hookRemoveUser"],                    // (done) Remove a user
-    ['/^web:\/?api\/user\/password\/reset\/?$/i',  "\Mod\User::hookPasswordReset"],                 // (done) Reset password to default
-    ['/^web:\/?api\/user\/password\/edit\/?$/i',   "\Mod\User::hookPasswordChange"],                // (done) Change password
-  
-    ['/^web:\/?api\/siteconfiguration\/set\/?$/i', "\Mod\SiteConfiguration::hookSetConfig"],        // (done) Set configuration
-  
-    ['/^web:\/?api\/collab_update\/?$/i',          "\Mod\Collaboration::hookUpdate"],               // (done) Collaboration status update
-  
-    ['/^web:\/?api\/page\/new\/?$/i',              "\Mod\Page::hookNewPage"],                       // (done)
-    ['/^web:\/?api\/page\/remove\/?$/i',           "\Mod\Page::hookRemovePage"],                    // (done)
-    ['/^web:\/?api\/page\/edit\/?$/i',             "\Mod\Page::hookEditPage"],                      // (done)
-    ['/^web:\/?api\/page\/secure\/?$/i',           "\Mod\Page::hookSecurePage"],                    // (done)
-    ['/^web:\/?api\/page\/checkpid\/?$/i',         "\Mod\Page::hookCheckPid"],                      // (done)
-      
-    ['/^web:\/?api\/contactform\/response\/?$/i',  "\Mod\ContactForm::hookFormResponse"],           // (done)
-  
-    ['/^web:\/?api\/notice\/checknid\/?$/i',       "\Mod\Notice::hookCheckNid"],                    // (done)
-    ['/^web:\/?api\/notice\/new\/?$/i',            "\Mod\Notice::hookNew"],                         // (done)
-    ['/^web:\/?api\/notice\/delete\/?$/i',         "\Mod\Notice::hookDelete"],                      // (done)
-  
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Page::hookMain"],                          // (done) Open and write page for remaining hooks (without extensions)
-  
-                                                                                                    // SecureMenu hooks
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Page::hookMenu"],                          // (done) Page module menu entries
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Collaboration::hookMenu"],                 // (done) Collaboration menu, and last-visited page update
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\User::hookMenu"],                          // (done)
-  
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Notice::hookMenu"],                        // (done) Notice module menu hook
-  
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\ModuleMenu::hookAddToSecureMenu"],         // (done)
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\SecureMenu::hookAboutMenu"],               // (done)
- 
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\SecureMenu::hook"],                        // (done) Display secure menu
- 
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Page::hookClose"],                         // (done) Close page
-    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Placeholders::hookEvaluatePlaceholders"],  // (done) Evaluate placeholders on page
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Page::hookMain"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Page::hookMenu"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Collaboration::hookMenu"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\User::hookMenu"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Notice::hookMenu"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\ModuleMenu::hookAddToSecureMenu"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\SecureMenu::hookAboutMenu"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\SecureMenu::hook"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Page::hookClose"],
+    ['/(?!.*\.[a-z]*$)^web:.*$/i',                 "\Mod\Placeholders::hookEvaluatePlaceholders"],
 ];
