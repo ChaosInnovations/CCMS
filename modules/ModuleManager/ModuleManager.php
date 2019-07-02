1794048a5834d1cc5806e0158a4ec7d10d159d31
Thomas Boland <thomas.boland@outlook.com>
2019-07-02 14:46:00 -0700

    ModuleManager progress

--- modules/ModuleManager/ModuleManager.php
+++ modules/ModuleManager/ModuleManager.php
@@ -3,12 +3,18 @@
 namespace Mod;
 
 use \Lib\CCMS\Request;
+use \Lib\CCMS\Response;
 use \Lib\CCMS\Utilities;
 use \Mod\ModuleMenu;
 use \Mod\SecureMenu;
 
 class ModuleManager {
-    public static function hookMenu(Request $request) {
+    public static function hookMenu(Request $request)
+    {
+        $availablePackageCache = self::getPackageInfoCache();
+        $packageCacheUpdated = strtotime($availablePackageCache["checked"]);
+        $packageCacheData = $availablePackageCache["packages"];
+
         $moduleModalEntryTemplate = file_get_contents(dirname(__FILE__) . "/templates/ModuleModalEntry.template.html");
 
         $moduleList = "";
@@ -26,12 +32,16 @@ class ModuleManager {
                 $dependencyList = "None";
             }
 
+            $hasUpdate = isset($availablePackageCache["updates"][$module_name]);
+            $uA_text = ($hasUpdate ? "&nbsp;<button class=\"btn btn-link\" title=\"Update Available\" style=\"padding: 0;\"><i class=\"fas fa-chevron-circle-up\"></i></button>" : "");
+
             $uninstall_button = '<button class="btn btn-outline-danger" title="Uninstall Module"><i class="fas fa-trash"></i></button>';
             if ($module["dependencies"]["has_dependent"]) {
                 $uninstall_button = '<button class="btn btn-outline-secondary" title="Other modules depend on this" disabled="disabled"><i class="fas fa-trash"></i></button>';
             }
 
             $template_vars = [
+                'ua_icon' => $uA_text,
                 'name' => $module["module_data"]["name"],
                 'description' => $module["module_data"]["description"],
                 'version' => implode(".", $module["module_data"]["version"]),
@@ -49,5 +59,98 @@ class ModuleManager {
         $moduleManagerBody = Utilities::fillTemplate(file_get_contents(dirname(__FILE__) . "/templates/ModuleModalBody.template.html"), $template_vars);
         SecureMenu::Instance()->addModal("dialog_modules", "Manage Modules", $moduleManagerBody, "");
         ModuleMenu::Instance()->addEntry("showDialog('modules');", "Manage Modules");
+    }
+
+    public static function hookUninstall(Request $request)
+    {
+
+    }
+
+    public static function hookInstall(Request $request)
+    {
+
+    }
+
+    public static function getPackageInfoCache() {
+        if (!file_exists(dirname(__FILE__) . "/packageInfoCache.json")) {
+            return ["checked" => "never", "packages" => []];
+        }
+
+        return json_decode(file_get_contents(dirname(__FILE__) . "/packageInfoCache.json"), true);
+    }
+
+    public static function hookCheckModules(Request $request) {
+        $SRV_URL = "http://ccms.thomasboland.me";
+        $VER_URL = $SRV_URL . "/latest.php?mod";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $VER_URL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        $out = curl_exec($ch);
+        curl_close($ch);
+
+        $data = [
+            "checked" => date("Y-m-d H:i:s"),
+            "packages" => json_decode($out, true),
+        ];
+
+        // Check for new packages and available updates
+        $newPackages = [];
+        $availableUpdates = [];
+        $moduleManifest = Utilities::getModuleManifest();
+        foreach ($data["packages"] as $pkg_name => $pkg_versions) {
+            if (!isset($moduleManifest[$pkg_name])) {
+                $newPackages[$pkg_name] = $pkg_versions;
+                continue;
+            }
+
+            $curVer = $moduleManifest[$pkg_name]["module_data"]["version"];
+            $availableUpdates[$pkg_name] = [];
+            foreach ($pkg_versions as $version_name => $pkg_data) {
+                // Check if package is newer than current package
+                $newVer = $pkg_data["module_data"]["version"];
+                $cmp = 8 * ($newVer[0] <=> $curVer[0]);
+                $cmp += 4 * ($newVer[1] <=> $curVer[1]);
+                $cmp += 2 * ($newVer[2] <=> $curVer[2]);
+                $cmp += 1 * ($newVer[3] <=> $curVer[3]);
+                if ($cmp <= 0) {
+                    continue;
+                }
+
+                $availableUpdates[$pkg_name][$version_name] = $pkg_data;
+            }
+        }
+
+        foreach ($availableUpdates as $pkg_name => $pkg_versions) {
+            if (count($pkg_versions) == 0) {
+                unset($availableUpdates[$pkg_name]);
+                continue;
+            }
+
+            uasort($availableUpdates[$pkg_name], function ($a, $b) {
+                $cmp = 8 * ($a["module_data"][0] <=> $b["module_data"][0]);
+                $cmp += 4 * ($a["module_data"][1] <=> $b["module_data"][1]);
+                $cmp += 2 * ($a["module_data"][2] <=> $b["module_data"][2]);
+                $cmp += 1 * ($a["module_data"][3] <=> $b["module_data"][3]);
+                return $cmp <=> 0;
+            });
+        }
+
+        foreach ($newPackages as $pkg_name => $pkg_versions) {
+            uasort($newPackages[$pkg_name], function ($a, $b) {
+                $cmp = 8 * ($a["module_data"][0] <=> $b["module_data"][0]);
+                $cmp += 4 * ($a["module_data"][1] <=> $b["module_data"][1]);
+                $cmp += 2 * ($a["module_data"][2] <=> $b["module_data"][2]);
+                $cmp += 1 * ($a["module_data"][3] <=> $b["module_data"][3]);
+                return $cmp <=> 0;
+            }, );
+        }
+
+        $data["new"] = $newPackages;
+        $data["updates"] = $availableUpdates;
+
+        file_put_contents(dirname(__FILE__) . "/packageInfoCache.json", json_encode($data));
+
+        return new Response(json_encode($data));
     }
 }
