ef1db5a8358cc89e3bf220dee8e66fbb06c1b07a
Thomas Boland <thomas.boland@outlook.com>
2019-07-22 20:07:26 -0700

    Moved FireSock to own repo

--- pkg/FireSock/ISubscription.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Package\FireSock;
-
-interface ISubscription {
-    public function __construct($server, $user, $hook);
-    public function processMessage($message);
-    public function tick();
-}
